7986ebba0828ce71fae1771ca6697c4ed4914b14
Winnie Jomo <winniejomo89@gmail.com>
2024-09-30 11:22:19 +0300

    completed

---
+++ ShopProject/settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'ShopProject',  
+        'USER': 'winniejomo',  
+        'PASSWORD': 'Jomo@123#',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
+    }
+}
+
